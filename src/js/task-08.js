e3c645f4b478b461d03ce2cb8c6ef331bf21d171
aprilespiritu <april_espiritu@live.com>
2024-04-10 19:36:18 +0800

    Update

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements['email'];
+  const passwordInput = loginForm.elements['password'];
+
+  if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
+    alert('All fields must be filled in.');
+    return;
+  }
+
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+});
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+
+  colorSpan.textContent = randomColor;
+});
+
